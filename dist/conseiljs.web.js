91edce3fe92f800d866014e477ff322f0b9d0b78
developer0623 <developer0623@yahoo.com>
2018-12-20 20:53:36 -0600

    testing

---
+++ dist/conseiljs.web.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./tezos/TezosConseilQuery"));
+__export(require("./tezos/TezosNodeQuery"));
+__export(require("./tezos/TezosOperations"));
+__export(require("./tezos/TezosWallet"));
+//# sourceMappingURL=conseiljs.web.js.map
